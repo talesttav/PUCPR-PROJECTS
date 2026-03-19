f02eeb1088b489770444aef4f254138ea50b7b2b
Tales Augusto Tavares <talestavaresco@gmail.com>
2026-03-19 20:55:35 -0300

    Add files via upload

---
+++ Conditional with Elif Project .py
@@ -0,0 +1,8 @@
+idade = int(input("Coloque a sua idade:\n"))
+if idade >=18 :
+  print("Você deve votar!")
+elif idade >15 :
+  print("Pode fazer cadastro para votar.") 
+else :
+  print("Você não pode votar!")
+  
