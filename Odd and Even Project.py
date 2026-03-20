cdc950dcb1e0b69166303f45d56266bc68dc4458
Tales Augusto Tavares <talestavaresco@gmail.com>
2026-03-19 21:10:11 -0300

    Add files via upload

---
+++ Odd and Even Project.py
@@ -0,0 +1,8 @@
+par = 0
+numero_1 = int(input("Digite 1° número:\n"))
+numero_2 = int(input("Digite 2° número:\n"))
+sobra = numero_1 % numero_2
+if sobra==par :
+    print("Par")
+else :
+    print("Impar")
