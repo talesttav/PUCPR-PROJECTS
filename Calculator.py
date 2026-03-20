ce368b0bec6e8719ca929274ebdc045fde6536fb
Tales Augusto Tavares <talestavaresco@gmail.com>
2026-03-19 21:47:35 -0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+numero_1 = int(input("Coloque 1° número:\n"))
+numero_2 = int(input("Coloque 2°número:\n"))
+import math
+soma = numero_1 + numero_2
+sub = numero_1 - numero_2
+div = numero_1 / numero_2
+mult = numero_1 * numero_2
+quadrado = numero_1 * numero_1
+raiz = math.sqrt (numero_1)
+print("Resultado da soma:\n", soma)
+print("Resultado da subtração:\n", sub)
+print("Resultado da divisão:\n", div)
+print("Resultado da multiplicação:\n", mult)
+print("Resultado da raiz:\n", raiz)
+print("Resultado do quadrado:\n", quadrado)
+
+
